38f6a8a9b7f7b8488a3c06e976b4db9f943818a2
Guille <g.mirandagaleote@gmail.com>
2024-03-18 21:23:26 +0100

    Add new logo and video, install react-simple-TypeWriter and update styles

---
+++ src/components/opening/opening.tsx
@@ -0,0 +1,27 @@
+import video_ej from '../../assets/video_ej.mp4'
+import logo_vector from '../../assets/logo_vector.png'
+import { useTypewriter, Cursor } from 'react-simple-typewriter' 
+
+export function Opening (){
+  const [text] = useTypewriter({
+    words: ['entrenamiento', 'rehabilitación', 'movilidad', 'trabajo duro', 'constancia', 'mejora'],
+    loop: 1000,
+    typeSpeed: 120,
+    deleteSpeed: 80
+  })
+
+  return(
+    <div className='video'>
+      <video src={video_ej} autoPlay loop muted/>
+      <div className='video_button'>
+        <button type='button'>
+          <div className='video_button_text'>Pulsa la imagen</div> 
+          <img src={logo_vector} />
+          <span> Somos {text}<Cursor cursorStyle= '<'/></span>
+          
+        </button>
+
+      </div>
+    </div>
+  )
+}
