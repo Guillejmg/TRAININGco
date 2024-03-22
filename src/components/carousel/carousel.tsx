37bab8ba6f870b80cf60ee52b2a83054cb3b2720
Guille <g.mirandagaleote@gmail.com>
2024-03-22 15:44:55 +0100

    Add favicon and basic carousel styles

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -1,10 +1,18 @@
 import './carousel.scss'
 import gym1 from '../../assets/gym1.jpg'
+import gym2 from '../../assets/gym2.jpg'
 const Carousel = () => {
   return(
     <>
       <div className="carousel">
-        <img className="carousel_img1" src={gym1} alt="imagen del gimnasio" />
+        <ul className='carousel__list'>
+          <li className='carousel__list-1'>
+            <img className="carousel__img1" src={gym1} alt="imagen del gimnasio 1" />
+          </li>
+          <li className='carousel__list-2'>
+            <img className="carousel__img2" src={gym2} alt="imagen del gimnasio 2" />
+          </li>
+        </ul>
       </div>
     </>
   )
