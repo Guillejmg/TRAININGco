6c5d6aeeb620d194ab0635d4036631f14c7ceef7
Guille <g.mirandagaleote@gmail.com>
2024-03-26 15:13:57 +0100

    Finish basic carousel

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -2,6 +2,9 @@ import './carousel.scss'
 import gym1 from '../../assets/gym1.jpg'
 import gym2 from '../../assets/gym2.jpg'
 const Carousel = () => {
+
+  // Para implantar un carusel interactuvo y  escalable con puntos debemos usar un archivo jason.db en assets, usar useRef (https://www.youtube.com/watch?v=oNROv1-ZLOY, https://www.youtube.com/watch?v=3Eg_V06pYn8 ), y luego usar un  map  que luego enlazaremos con  la logica  de .forEach() (https://www.youtube.com/watch?v=2CEptqw-bSQ)
+  
   return(
     <>
       <div className="carousel">
@@ -12,7 +15,15 @@ const Carousel = () => {
           <li className='carousel__list-2'>
             <img className="carousel__img2" src={gym2} alt="imagen del gimnasio 2" />
           </li>
+          <li className='carousel__list-3'>
+            <img className="carousel__img3" src={gym1} alt="imagen del gimnasio 3" />
+          </li>
         </ul>
+        {/* <ul className="carousel__point" ref= {pictureList}>
+          <li className="carousel__point-size" onClick={handlePicture} ref= {picturePoint}></li>
+          <li className="carousel__point-size" onClick={handlePicture} ref= {picturePoint}></li>
+          <li className="carousel__point-size" onClick={handlePicture} ref= {picturePoint}></li>
+        </ul> */}
       </div>
     </>
   )
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -6,11 +6,21 @@ const Navbar = ()=>{
   return (
     <>
       <ul  className='navbar'>
-        <li><Link className='navbar_link' to="/home">HOME</Link></li>
-        <li><Link className='navbar_link' to="/services">QUÉ OFRECEMOS</Link></li>
-        <li><Link className='navbar_link' to="/us">NOSOTROS</Link></li>
-        <li><Link className='navbar_link' to="/contact">CONTÁCTANOS</Link></li>
-        <li className='navbar_img'><img  src={IMG_1335} alt="TRAININGco" /></li>
+        <li>
+          <Link className='navbar_link' to="/home">HOME</Link>
+        </li>
+        <li>
+          <Link className='navbar_link' to="/services">QUÉ OFRECEMOS</Link>
+        </li>
+        <li>
+          <Link className='navbar_link' to="/us">NOSOTROS</Link>
+        </li>
+        <li>
+          <Link className='navbar_link' to="/contact">CONTÁCTANOS</Link>
+        </li>
+        <li className='navbar_img'>
+          <img  src={IMG_1335} alt="TRAININGco" />
+        </li>
       </ul>
     </>
   )
