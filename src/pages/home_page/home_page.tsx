e5de0c9663c4f4dddb9d1a2d99ff781b2d51aef1
Guille <g.mirandagaleote@gmail.com>
2024-04-07 18:31:20 +0200

    Update and optimize images

--- src/pages/home_page/home_page.tsx
+++ src/pages/home_page/home_page.tsx
@@ -1,4 +1,3 @@
-//import { useState, useEffect } from 'react';
 import Header from "../../components/header/header"
 import Carousel from '../../components/carousel/carousel';
 import './home_page.scss'
@@ -6,9 +5,40 @@ import './home_page.scss'
 const HomePage = () => {
 
   return (
-    <div className="home_page">
+    <div className="home-page">
       <Carousel></Carousel>
       <Header/>
+      <p>a</p>
+      <p>b</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>a
+      <p>a</p>
+      <p></p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+
     </div>
   )
 }
